5042d2a14c8cf76fa28bdbd274a41d111724d2f2
knromaric <nzekeng@gmail.com>
2018-11-16 12:51:07 +0200

    Added infoPage functionalities

--- CoffeeRoom/CoffeeRoom/Pages/InfoPage.xaml.cs
+++ CoffeeRoom/CoffeeRoom/Pages/InfoPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +16,30 @@ namespace CoffeeRoom.Pages
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void TapFacebook_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.facebook.com/"));
+        }
+
+        private void TapTwitter_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://twitter.com/"));
+        }
+
+        private void TapInstagram_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.instagram.com/"));
+        }
+
+        private void TapYoutube_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.youtube.com/watch?v=aBLYSO0DSVI"));
+        }
+
+        private void TapPhone_Tapped(object sender, EventArgs e)
+        {
+            PhoneDialer.Open("992998372");
+        }
+    }
 }
